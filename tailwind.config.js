c922b9fb1ed54f335ac9f9e8ba8ce4abded11573
00lar <eduardani84@gmail.com>
2023-01-30 01:48:18 -0700

    v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        white: "hsl(0, 0%, 100%)",
+        gray: "hsl(212, 45%, 89%)",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+      textColor: {
+        Dark_Blue: "hsl(218, 44%, 22%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+      },
+    },
   },
   plugins: [],
-}
+};
